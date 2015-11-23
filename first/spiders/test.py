2fede1c7793be9da081d2225de9a0596e78be659
Alex Krylov <ganggbang@mail.ru>
2015-11-23 13:27:01 +0300

    follow page

--- first/spiders/test.py
+++ first/spiders/test.py
@@ -28,13 +28,18 @@ class TestSpider(CrawlSpider):
     allowed_domains = ["ebay.com", "copart.com", "www.manheimglobaltrader.com", "boattrader.com"]
     start_urls = [#"https://www.manheimglobaltrader.com/bu/search?se_search_unit_code[]=BO&flag_search_submit=y",
                 #"http://www.copart.com/us/search?companyCode_vf=US&Sort=sd&LotTypes=M&YearFrom=2000&YearTo=2016&Make=&RadioGroup=Location&YardNumber=&States=&PostalCode=&Distance=500&searchTitle=2000-2016%2C%2C&cn=2000-2016%2C%2C",
-                #"http://www.ebay.com/sch/Boats-/26429/i.html?rt=nc&LH_BIN=1&_trksid=p2045573.m1684",
-                "http://www.boattrader.com/search-results/NewOrUsed-any/Type-all/Category-all/Radius-200/Sort-Length:DESC",]
+                "http://www.ebay.com/sch/Boats-/26429/i.html?rt=nc&LH_BIN=1&_trksid=p2045573.m1684",
+                #"http://www.boattrader.com/search-results/NewOrUsed-any/Type-small+boats/Category-all/Radius-200/Sort-Length:DESC",
+                ]
     rules = (
-        #Rule(SgmlLinkExtractor(restrict_xpaths = ('//h3[@class=\'lvtitle\']/a')), callback = 'parse_item_ebay', follow = False),
+        Rule(SgmlLinkExtractor(restrict_xpaths = ('//h3[@class=\'lvtitle\']/a')), callback = 'parse_item_ebay'),
+        Rule(SgmlLinkExtractor(restrict_xpaths = ('//td[@class=\'pagn-next\']/a')), follow=True),
+        #
         #Rule(SgmlLinkExtractor(restrict_xpaths = ('//li[@class=\'lot-desc\']/a')), callback = 'parse_item_copart'),
         #Rule(SgmlLinkExtractor(restrict_xpaths = ('//td/table[@class=\'search_name_cell\']/tr/td/a')), callback = 'parse_item_manheimglobaltrader'),
-        Rule(SgmlLinkExtractor(restrict_xpaths = ('//div[@class=\'inner\']/a')), callback = 'parse_item_boattrader'),
+        #Rule(SgmlLinkExtractor(restrict_xpaths = ('//div[@class=\'inner\']/a')), callback = 'parse_item_boattrader'),
+        #Rule(SgmlLinkExtractor(restrict_xpaths = ('//a[contains(text(),\'>\')]')), follow=True),
+        #
     )
 
     def list_xpath_to_str(self, charact, xpath):
@@ -288,7 +293,7 @@ class TestSpider(CrawlSpider):
             price = re.sub('[^\d+]','',txt)
         else:
             price = 0
-        print price
+        #print price
         #return price
 
         return price
@@ -337,7 +342,7 @@ class TestSpider(CrawlSpider):
         imgs = sel.xpath('//img[contains(@src,\'http://images0.boattrader.com\')]')
         items = []
 
-        print imgs
+        #print imgs
         img_index = 0
 
         for img in imgs:
@@ -356,17 +361,18 @@ class TestSpider(CrawlSpider):
             item[item_url_index] = img.xpath('@src').extract()
 
             m = re.search('.*.jpg',item[item_url_index][0])
-            item[item_url_index][0] = m.group(0)
-            #print item[item_image_index]
-            tmp = ''.join(item[item_image_index])
-            m = re.search('\w+\.com',tmp)
             if m:
-                item['name'] = m.group(0)
+                item[item_url_index][0] = m.group(0)
+                #print item[item_image_index]
+                tmp = ''.join(item[item_image_index])
+                m = re.search('\w+\.com',tmp)
                 if m:
-                    m = re.search('(\/[0-9,a-z\-\_]+\/[0-9,a-z\-\_]+|[0-9,a-z\-\_]+).jpg',item[item_image_index][0].lower())
-                    item[item_image_index] = "%s/%s" % (item['name'], m.group(0))
-                    item[item_image_index] = re.sub('//','/',item[item_image_index])
-                    img_index += 1
+                    item['name'] = m.group(0)
+                    if m:
+                        m = re.search('(\/[0-9,a-z\-\_]+\/[0-9,a-z\-\_]+|[0-9,a-z\-\_]+).jpg',item[item_image_index][0].lower())
+                        item[item_image_index] = "%s/%s" % (item['name'], m.group(0))
+                        item[item_image_index] = re.sub('//','/',item[item_image_index])
+                        img_index += 1
 
         items.append(item)
 
