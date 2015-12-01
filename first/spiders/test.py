774a38c8ecf3652aa27a0ec258523401776be6ae
Alex Krylov <ganggbang@mail.ru>
2015-12-01 10:50:35 +0300

    fixed

--- first/spiders/test.py
+++ first/spiders/test.py
@@ -27,17 +27,17 @@ class TestSpider(CrawlSpider):
     name = 'scraby'
     allowed_domains = ["ebay.com", "copart.com", "www.manheimglobaltrader.com", "boattrader.com"]
     start_urls = [
-                "https://www.manheimglobaltrader.com/bu/search?se_search_unit_code[]=BO&flag_search_submit=y&offset=1&limit=500",
-                #"http://www.copart.com/us/search?companyCode_vf=US&Sort=sd&LotTypes=M&YearFrom=2000&YearTo=2016&Make=&RadioGroup=Location&YardNumber=&States=&PostalCode=&Distance=500&searchTitle=2000-2016%2C%2C&cn=2000-2016%2C%2C",
+                #"https://www.manheimglobaltrader.com/bu/search?se_search_unit_code[]=BO&flag_search_submit=y&offset=1&limit=500",
+                "http://www.copart.com/us/search?companyCode_vf=US&Sort=sd&LotTypes=M&YearFrom=2000&YearTo=2016&Make=&RadioGroup=Location&YardNumber=&States=&PostalCode=&Distance=500&searchTitle=2000-2016%2C%2C&cn=2000-2016%2C%2C",
                 #"http://www.ebay.com/sch/Boats-/26429/i.html?rt=nc&LH_BIN=1&_trksid=p2045573.m1684",
                 #"http://www.boattrader.com/search-results/NewOrUsed-any/Type-small+boats/Category-all/Radius-200/Sort-Length:DESC",
                 ]
     rules = (
          #Rule(SgmlLinkExtractor(restrict_xpaths = ('//h3[@class=\'lvtitle\']/a')), callback = 'parse_item_ebay'),
          # Rule(SgmlLinkExtractor(restrict_xpaths = ('//td[@class=\'pagn-next\']/a')), follow=True),
-          #Rule(SgmlLinkExtractor(restrict_xpaths = ('//li[@class=\'lot-desc\']/a')), callback = 'parse_item_copart'),
+          Rule(SgmlLinkExtractor(restrict_xpaths = ('//li[@class=\'lot-desc\']/a')), callback = 'parse_item_copart'),
          # Rule(SgmlLinkExtractor(restrict_xpaths = ('//a[@class=\'pager-next\']')), follow=True),
-         Rule(SgmlLinkExtractor(restrict_xpaths = ('//table[@class=\'search_list_container\']/tr/td[1]/table/tr/td/a')), callback = 'parse_item_manheimglobaltrader'),
+         #Rule(SgmlLinkExtractor(restrict_xpaths = ('//table[@class=\'search_list_container\']/tr/td[1]/table/tr/td/a')), callback = 'parse_item_manheimglobaltrader'),
          # Rule(SgmlLinkExtractor(restrict_xpaths = ('(//input[@value=\'Next\'])[2]')), follow=True),
          # Rule(SgmlLinkExtractor(restrict_xpaths = ('//li/div[@class=\'inner\']/a')), callback = 'parse_item_boattrader'),
          # Rule(SgmlLinkExtractor(restrict_xpaths = ('//a[contains(text(),\'>\')]')), follow=True),
@@ -281,8 +281,8 @@ class TestSpider(CrawlSpider):
                     m = re.search('(\/[0-9,a-z\-\_]+\/[0-9,a-z\-\_]+|[0-9,a-z\-\_]+).jpg$',item[item_image_index][0], flags=re.IGNORECASE)
                     item[item_image_index] = "/tmp/%s/%s" % (item['name'], m.group(0))
                     item[item_image_index] = re.sub('//','/',item[item_image_index])
-                    item[item_image_index] = re.sub('.jpg$','x.jpg',item[item_image_index], flags=re.IGNORECASE)
-                    item[item_url_index] = re.sub('.jpg$','x.jpg',item[item_url_index][0], flags=re.IGNORECASE)
+                    item[item_image_index] = re.sub('.jpg$','X.JPG',item[item_image_index], flags=re.IGNORECASE)
+                    item[item_url_index] = re.sub('.jpg$','X.JPG',item[item_url_index][0], flags=re.IGNORECASE)
                     #print item[item_url_index]
                     img_index += 1
         items.append(item)
